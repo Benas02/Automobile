59522e90ebb430206831ea12d9997db0a9812c48
Benas02 <gabriele.rubiera@gmail.com>
2023-10-12 12:05:06 +0200

    App Automobile

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* L'App deve modellare un'Automobile, per farlo deve registrare le informazioni relative a:
+ * - MARCA
+ * - MODELLO
+ * - TIPO DI MOTORE (BENZINA, DIESEL, ELETTRICO)
+ * - COLORE
+ * 
+ * All'avvio l'app deve mostrare lo stato dell'Auto, stampando marca, modello, tipo di motore e colore.
+ * Per modellare 
+*/
+
+class Auto {
+    
+}
